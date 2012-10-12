fb847771a68c9bfdd200c73fcd59873015bf5835
Ben Phelps <bphelpsen@gmail.com>
2012-10-12 17:25:27 -0500

    Clean up and remove some menu items.

--- TradingView/AppDelegate.m
+++ TradingView/AppDelegate.m
@@ -10,35 +10,37 @@
 
 @implementation AppDelegate
 @synthesize tradingview;
+@synthesize extraview;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     [tradingview setFrameLoadDelegate: self];
     [tradingview setPolicyDelegate: self];
     
+    [[tradingview layer] setCornerRadius:10];
+    
     // Load the page in
     NSString *urlString = @"https://www.tradingview.com/e/";
     [[tradingview mainFrame]loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(webViewDidFinishLoad:)
+                                                 name:WebViewProgressFinishedNotification
+                                               object:tradingview];
+    
+}
+
+-(void) webViewDidFinishLoad:(WebView *)webView
+{
+    [tradingview stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('tweetimage')[0].style.display = 'none';"];
 }
 
 - (void)webView:(WebView *)sender didReceiveTitle:(NSString *)pageTitle forFrame:(WebFrame *)frame {
     // there is a blip of "AddThis utility frame" and I'm not sure why, so we block it
     if([pageTitle rangeOfString:@"AddThis"].location == NSNotFound){
         [self updateWindowTitle:pageTitle];
-    }  
-}
-
-- (void)webView:(WebView *)webView
-decidePolicyForNavigationAction:(NSDictionary *)actionInformation
-                        request:(NSURLRequest *)request frame:(WebFrame *)frame
-               decisionListener:(id < WebPolicyDecisionListener >)listener
-{
-    NSString *host = [[request URL] host];
-    if ([host rangeOfString:@"www.tradingview.com"].location == NSNotFound) {
-        // stop loading external shit
-    } else {
-        [listener use];
     }
+    
 }
 
 - (void)webView:(WebView *)webView
@@ -58,6 +60,7 @@ decidePolicyForNewWindowAction:(NSDictionary *)actionInformation
 
 - (void) updateWindowTitle: (NSString*) title {
 	[[tradingview window]setTitle:title];
+    [[extraview window]setTitle:title];
 }
 
 @end
